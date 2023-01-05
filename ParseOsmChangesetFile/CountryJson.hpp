7b5653c9c70debb33947834e3c85e9e4326e33a3
Bryce Cogswell <bryceco@yahoo.com>
2023-01-04 20:52:08 -0800

    Move geojson data into it's own file

---
+++ ParseOsmChangesetFile/CountryJson.hpp
@@ -0,0 +1,14 @@
+//
+//  CountryJson.hpp
+//  ParseOsmChangesetFile
+//
+//  Created by Bryce Cogswell on 1/4/23.
+//  Copyright © 2023 Bryce Cogswell. All rights reserved.
+//
+
+#ifndef CountryJson_hpp
+#define CountryJson_hpp
+
+extern const char * countryJson;
+
+#endif /* CountryJson_hpp */
